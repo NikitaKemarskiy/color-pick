5a5592c6d2335654835e1608220a281ce5b9c2e1
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-09-15 18:20:34 +0300

    The basic color picking is ready.

--- index.js
+++ index.js
@@ -2,35 +2,45 @@
 const path = require('path');
 
 // Libs
-const { getColors, getTop, getDifference } = require(path.join(__dirname, 'libs', 'colors'));
+const { getColors, getDifference, colorsToMap } = require(path.join(__dirname, 'libs', 'colors'));
 
 // Constants
-const IMG_PATH = path.join(__dirname, 'img', 'audi.jpg');
+const IMG_PATH = path.join(__dirname, 'img', 'mercedes.jpg');
+const MAX_DIFFERENCE = 90; // Max difference of colors
 
 async function main() {
     const colors = await getColors(IMG_PATH); // Get the colors array
-    const stats = new Map(); // Create stats Map
+    const stats = colorsToMap(colors); // Create stats Map
 
-    for (let i = 0; i < colors.length - 1; i++) {
-        if (stats.has(colors[i].number)) { // Color is already in Map
-            const data = stats.get(colors[i].number); // Get value object
-            data.count++; // Increment count (number of repeats)
-        } else {
-            const { red, green, blue } = colors[i];
-            stats.set(colors[i].number, { // Set value object
-                red,
-                green,
-                blue,
-                count: 1
-            });
+    const statsSort = [...stats].sort((a, b) => {
+        return a[0] - b[0];
+    });
+
+    for (let i = 1; i < statsSort.length; i++) {
+        const difference = getDifference(statsSort[i][1], statsSort[i - 1][1]);
+        if (difference <= MAX_DIFFERENCE) {
+            statsSort.splice(i, 1);
+            i--;
         }
     }
 
-    const top = getTop(stats, 2000); // Get top colors
+    for (let i = 0; i < statsSort.length - 1; i++) {
+        for (let j = i + 1; j < statsSort.length; j++) {
+            const difference = getDifference(statsSort[i][1], statsSort[j][1]);
+            if (difference <= MAX_DIFFERENCE) {
+                statsSort.splice(j, 1);
+                j--;
+            }
+        }
+    }
 
-    top.forEach((item) => {
-        console.log(item);
-    });
+    console.dir(statsSort);
+
+    // const top = getAverage(stats, NUMBER_OF_COLORS, MAX_DIFFERENCE); // Get top colors
+
+    // top.forEach((item) => {
+    //     console.log(item);
+    // });
 }
 
 main();
--- libs/colors.js
+++ libs/colors.js
@@ -4,19 +4,24 @@ const util = require('util');
 const jpeg = require('jpeg-js');
 
 const { promisify } = util;
-const { sqrt, pow, floor } = Math;
+const { sqrt, pow, floor, ceil } = Math;
 
-// Get colors from JPEG image
+// Constants
+const COEFFICIENT = 4e5; // Coefficient of pixels to skip
+
+/*
+ * Get colors from JPEG image
+*/
 async function getColors(imgPath) {
     const imgRaw = await promisify(fs.readFile)(imgPath); // Get raw buffer
     const imgData = jpeg.decode(imgRaw); // Decode it into JPEG data buffer
     const bytes = imgData.data; // Get bytes
-
+    const { width, height } = imgData;
+    const skip = ceil(width * height / COEFFICIENT);
     const colors = [];
-
-    for (let i = 0; i < bytes.length; i += 4) {
+    for (let i = 0; i < bytes.length; i += 4 * (skip + 1)) {
         // Count int number of RGB
-        const number = bytes[i] * pow(255, 2) +
+        const number = bytes[i] * 255 * 255 +
                        bytes[i + 1] * 255 +
                        bytes[i + 2];
         const red = bytes[i]; // Red byte
@@ -29,30 +34,12 @@ async function getColors(imgPath) {
             blue
         });
     }
-
     return colors;
 }
 
-// Get top colors from the stats Map
-function getTop(stats, amount) {
-    const top = []; // Array of the top colors
-    for (let i = 0; i < amount; i++) {
-        let maxCount = 0;
-        let maxKey;
-        stats.forEach((value, key) => { // For every color
-            if (value.count > maxCount) { // Count of current color is greater than maxCount
-                maxCount = value.count; // Reassign maxCount
-                maxKey = key; // Reassign maxKey
-            }
-        });
-        const color = stats.get(maxKey); // Get maxCount color data
-        stats.delete(maxKey); // Delete this color
-        top.push(Object.assign(color, { number: maxKey })); // Insert obj with color data into the top array
-    }
-    return top;
-}
-
-// Get euclidean distance between two colors
+/*
+ * Get euclidean distance between two colors
+*/
 function getDifference(colorOne, colorTwo) {
     // Count euclidean distance
     const difference = sqrt(pow(colorOne.red - colorTwo.red, 2) +
@@ -61,9 +48,32 @@ function getDifference(colorOne, colorTwo) {
     return floor(difference);
 }
 
+/*
+ * Convert colors array to map where
+ * key is number and value is data
+*/
+function colorsToMap(colors) {
+    const stats = new Map(); // Create stats Map
+    for (let i = 0; i < colors.length - 1; i++) {
+        if (stats.has(colors[i].number)) { // Color is already in Map
+            const data = stats.get(colors[i].number); // Get value object
+            data.count++; // Increment count (number of repeats)
+        } else {
+            const { red, green, blue } = colors[i];
+            stats.set(colors[i].number, { // Set value object
+                red,
+                green,
+                blue,
+                count: 1
+            });
+        }
+    }
+    return stats;
+}
+
 // Exports
 module.exports = {
     getColors,
-    getTop,
-    getDifference
+    getDifference,
+    colorsToMap
 };
