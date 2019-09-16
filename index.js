e1610d1f804d08aa2e0205fe5fc65269643f7500
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-09-16 17:14:59 +0300

    Median cut algorithm for defining palette of colors was implemented.

--- index.js
+++ index.js
@@ -2,45 +2,14 @@
 const path = require('path');
 
 // Libs
-const { getColors, getDifference, colorsToMap } = require(path.join(__dirname, 'libs', 'colors'));
+const { getColors, pickColors, getDifference } = require(path.join(__dirname, 'libs', 'colors'));
 
 // Constants
-const IMG_PATH = path.join(__dirname, 'img', 'mercedes.jpg');
-const MAX_DIFFERENCE = 90; // Max difference of colors
+const IMG_PATH = path.join(__dirname, 'img', 'colorful.jpg');
 
 async function main() {
     const colors = await getColors(IMG_PATH); // Get the colors array
-    const stats = colorsToMap(colors); // Create stats Map
-
-    const statsSort = [...stats].sort((a, b) => {
-        return a[0] - b[0];
-    });
-
-    for (let i = 1; i < statsSort.length; i++) {
-        const difference = getDifference(statsSort[i][1], statsSort[i - 1][1]);
-        if (difference <= MAX_DIFFERENCE) {
-            statsSort.splice(i, 1);
-            i--;
-        }
-    }
-
-    for (let i = 0; i < statsSort.length - 1; i++) {
-        for (let j = i + 1; j < statsSort.length; j++) {
-            const difference = getDifference(statsSort[i][1], statsSort[j][1]);
-            if (difference <= MAX_DIFFERENCE) {
-                statsSort.splice(j, 1);
-                j--;
-            }
-        }
-    }
-
-    console.dir(statsSort);
-
-    // const top = getAverage(stats, NUMBER_OF_COLORS, MAX_DIFFERENCE); // Get top colors
-
-    // top.forEach((item) => {
-    //     console.log(item);
-    // });
+    const picked = pickColors(colors, 16);
 }
 
 main();
--- libs/colors.js
+++ libs/colors.js
@@ -4,10 +4,7 @@ const util = require('util');
 const jpeg = require('jpeg-js');
 
 const { promisify } = util;
-const { sqrt, pow, floor, ceil } = Math;
-
-// Constants
-const COEFFICIENT = 4e5; // Coefficient of pixels to skip
+const { sqrt, pow, floor, max } = Math;
 
 /*
  * Get colors from JPEG image
@@ -16,10 +13,9 @@ async function getColors(imgPath) {
     const imgRaw = await promisify(fs.readFile)(imgPath); // Get raw buffer
     const imgData = jpeg.decode(imgRaw); // Decode it into JPEG data buffer
     const bytes = imgData.data; // Get bytes
-    const { width, height } = imgData;
-    const skip = ceil(width * height / COEFFICIENT);
     const colors = [];
-    for (let i = 0; i < bytes.length; i += 4 * (skip + 1)) {
+    const unique = new Set();
+    for (let i = 0; i < bytes.length; i += 4) {
         // Count int number of RGB
         const number = bytes[i] * 255 * 255 +
                        bytes[i + 1] * 255 +
@@ -27,14 +23,56 @@ async function getColors(imgPath) {
         const red = bytes[i]; // Red byte
         const green = bytes[i + 1]; // Green byte
         const blue = bytes[i + 2]; // Blue byte
+        if (unique.has(number)) { continue; }
         colors.push({ // Push color into the colors array
-            number,
             red,
             green,
             blue
         });
+        unique.add(number);
     }
     return colors;
+}
+
+/*
+ * Recursive function for picking average
+ * palette of colors from the array.
+ * Factor parameter is number of colors
+ * that should be in final palette
+*/
+function pickColors(colors, factor) {
+    if (colors.length === 1) { return colors; } // Colors has one item -> return it
+    if (colors.length === 0) { return null; } // Colors has no items -> return null
+    if (factor === 1) { return [ getAverageColor(colors) ]; } // Factor is 1 -> return average color
+
+    const red = { min: 255, max: 0 };
+    const green = { min: 255, max: 0 };
+    const blue = { min: 255, max: 0 };
+    colors.forEach((color) => {
+        if (color.red < red.min) { red.min = color.red };
+        if (color.red > red.max) { red.max = color.red };
+        if (color.green < green.min) { green.min = color.green };
+        if (color.green > green.max) { green.max = color.green };
+        if (color.blue < blue.min) { blue.min = color.blue };
+        if (color.blue > blue.max) { blue.max = color.blue };
+    });
+    const redDiff = red.max = red.min;
+    const greenDiff = green.max = green.min;
+    const blueDiff = blue.max = blue.min;
+    switch (max(redDiff, greenDiff, blueDiff)) {
+        case redDiff: {
+            sortByColor(colors, 'red');
+        }
+        case greenDiff: {
+            sortByColor(colors, 'green');
+        }
+        case blueDiff: {
+            sortByColor(colors, 'blue');
+        }
+    }
+    const arrOne = pickColors(colors.slice(0, colors.length / 2), floor(factor / 2));
+    const arrTwo = pickColors(colors.slice(colors.length / 2), floor(factor / 2));
+    return arrOne.concat(arrTwo);
 }
 
 /*
@@ -49,31 +87,48 @@ function getDifference(colorOne, colorTwo) {
 }
 
 /*
- * Convert colors array to map where
- * key is number and value is data
+ * Sort colors array by the specified color
 */
-function colorsToMap(colors) {
-    const stats = new Map(); // Create stats Map
-    for (let i = 0; i < colors.length - 1; i++) {
-        if (stats.has(colors[i].number)) { // Color is already in Map
-            const data = stats.get(colors[i].number); // Get value object
-            data.count++; // Increment count (number of repeats)
-        } else {
-            const { red, green, blue } = colors[i];
-            stats.set(colors[i].number, { // Set value object
-                red,
-                green,
-                blue,
-                count: 1
+function sortByColor(colors, color) {
+    switch (color) {
+        case 'red': {
+            colors.sort((colorOne, colorTwo) => {
+                return colorOne.red - colorTwo.red;
+            });
+        }
+        case 'green': {
+            colors.sort((colorOne, colorTwo) => {
+                return colorOne.green - colorTwo.green;
+            });
+        }
+        case 'blue': {
+            colors.sort((colorOne, colorTwo) => {
+                return colorOne.blue - colorTwo.blue;
             });
         }
     }
-    return stats;
+}
+
+/*
+ * Function that returns average color
+ * from the colors array
+*/
+function getAverageColor(colors) {
+    const average = { red: 0, green: 0, blue: 0 };
+    colors.forEach((color) => {
+        average.red += color.red;
+        average.green += color.green;
+        average.blue += color.blue;
+    });
+    average.red = floor(average.red / colors.length);
+    average.green = floor(average.green / colors.length);
+    average.blue = floor(average.blue / colors.length);
+    return average;
 }
 
 // Exports
 module.exports = {
     getColors,
     getDifference,
-    colorsToMap
+    pickColors
 };
