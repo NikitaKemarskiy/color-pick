ecf0df409c291dad785a44d0f921a2767f0903e3
metamodern <shabanovbunya@gmail.com>
2019-09-16 22:54:43 +0300

    Made a script to alert the color
    now will make a script to transform rgb to hex

--- public/assets/scripts/copy_color.js
+++ public/assets/scripts/copy_color.js
@@ -1,16 +1,20 @@
-function copyToClipboard() {
-  // var $temp = $("<input>");
-  // $("body").append($temp);
-  // $temp.val($(element).text()).select();
-  // document.execCommand("copy");
-  // $temp.remove();
-
-
-  var x = event.clientX, y = event.clientY;
-  var elementMouseIsOver = document.elementFromPoint(x, y);
-  var style = window.getComputedStyle(elementMouseIsOver);
-  var color = style.getPropertyValue('background-color');
-  //alert(color);
-  // var dummy = $('<input>').val(color).appendTo('body').select();
-  // document.execCommand('copy');
+function copyToClip() {
+  const x = event.clientX, y = event.clientY;
+  const elementMouseIsOver = document.elementFromPoint(x, y);
+  
+  if (elementMouseIsOver.className == 'color') {
+    const style = window.getComputedStyle(elementMouseIsOver);
+    const color = style.getPropertyValue('background-color');
+    alert(color);
+  }
+  else if (elementMouseIsOver.className == 'color_text') {
+    const realcolor = elementMouseIsOver.parentNode;
+    const style = window.getComputedStyle(realcolor);
+    const color = style.getPropertyValue('background-color');
+    alert(color);
+  }
+  else if (elementMouseIsOver.className == 'colors') {
+    return;
+  }  
 }
+  
