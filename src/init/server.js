bd04283907b41c00a6e0a7065e45db391e484689
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-10-08 22:59:43 +0300

    Https was commented for easier development.

--- src/init/server.js
+++ src/init/server.js
@@ -9,19 +9,19 @@ const { promisify } = require('util');
 const config = require(path.join(__dirname, '..', '..', 'config', 'config'));
 
 // SSL
-const SSL = {
+/*const SSL = {
 	key: fs.readFileSync(config.ssl.keyPath),
 	cert: fs.readFileSync(config.ssl.certPath),
 	ca: fs.readFileSync(config.ssl.chainPath)
-};
+};*/
 
 // Server init function
 async function init(app) {
 	const httpServer = http.createServer(app.callback()); // Http server
-	const httpsServer = https.createServer(SSL, app.callback()); // Https server
+	// const httpsServer = https.createServer(SSL, app.callback()); // Https server
 	try {
 		await promisify(httpServer.listen).call(httpServer, 80);
-		await promisify(httpsServer.listen).call(httpsServer, 443);
+		// await promisify(httpsServer.listen).call(httpsServer, 443);
 		console.log(`=> Server was initialized!`);
 	} catch (err) {
 		console.error(err);
