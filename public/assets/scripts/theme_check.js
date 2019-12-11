1bef5b117bcf464eaf2ffd466d2c5f2fb5e3eadb
metamodern <shabanovbunya@gmail.com>
2019-12-11 23:56:01 +0200

    made theme saving in local storage
    would be great to set themes in another css file so the structure is always the same.

--- public/assets/scripts/theme_check.js
+++ public/assets/scripts/theme_check.js
@@ -1,20 +1,49 @@
-function checker(){
-  let isLight = document.getElementById('theme');
-  isLight.checked ? setLight(isLight) : setDark(isLight);  
+$(document).ready(function () {
+  const themeSwitch = document.getElementById('theme');
+    initTheme(); // on page load, if user has already selected a specific theme -> apply it
+    themeSwitch.addEventListener('change', function (event) {
+      checker(themeSwitch); // update color theme
+    });
+  function initTheme() {
+    var darkThemeSelected;
+    switch (localStorage.getItem('themeSwitch')) {
+      case null:
+        darkThemeSelected = true;
+        themeSwitch.checked = false;
+        break;
+      case 'dark':
+        darkThemeSelected = true;
+        themeSwitch.checked = false;
+        break;
+      case 'light':
+        darkThemeSelected = false;
+        themeSwitch.checked = true;
+        break;
+      default:
+        break;
+    }
+    // update body data-theme attribute
+    darkThemeSelected === true ? setDark(themeSwitch) : setLight(themeSwitch);
+  };
+});
+
+function checker(themeSwitch) {
+  themeSwitch.checked ? setLight(themeSwitch) : setDark(themeSwitch);
 }
 
 
-function setLight(isLight) {
-  isLight.checked = true;
+function setLight(themeSwitch) {
+  themeSwitch.checked = true;
   document.getElementById('worksheet').setAttribute('href', 'assets/styles/worksheet_style_light.css');
   document.getElementById('form').setAttribute('href', 'assets/styles/form_style_light.css');
   document.getElementById('submenu').setAttribute('href', 'assets/styles/submenu_light.css');
-
+  localStorage.setItem('themeSwitch', 'light');
 }
 
-function setDark(isLight) {
-  isLight.checked = false;
+function setDark(themeSwitch) {
+  themeSwitch.checked = false;
   document.getElementById('worksheet').setAttribute('href', 'assets/styles/worksheet_style.css');
   document.getElementById('form').setAttribute('href', 'assets/styles/form_style.css');
   document.getElementById('submenu').setAttribute('href', 'assets/styles/submenu.css');
+  localStorage.setItem('themeSwitch', 'dark');
 }
